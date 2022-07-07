f2974dde8e22075a762d71153c47956beeee4263
IvanDanilov03 <ivan_danilov@ukr.net>
2022-07-08 00:58:41 +0300

    Updates

--- js/app.js
+++ js/app.js
@@ -298,7 +298,7 @@ __webpack_require__.r(__webpack_exports__);
 // extracted by mini-css-extract-plugin
 
     if(true) {
-      // 1657228252152
+      // 1657231118985
       var cssReload = __webpack_require__(/*! ./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js */ "./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js")(module.id, {"locals":false});
       module.hot.dispose(cssReload);
       module.hot.accept(undefined, cssReload);
@@ -377,7 +377,7 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("2b6551e59b90acbd6ca8")
+/******/ 		__webpack_require__.h = () => ("9c9e795aa5fd0278dbc0")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
